96d3d19258342306dde50efd8f37fcca92062760
nthGH1030 <nth1030@gmail.com>
2025-12-18 23:53:20 +0800

    feta: align the line space of qualification

--- src/components/QualificationCard.tsx
+++ src/components/QualificationCard.tsx
@@ -13,14 +13,14 @@ const QualificationCard: React.FC<QualifcationProps> = ({title, date, img}) => {
             </div>
              <div className="md:hidden rounded-lg bg-gray-200/50 p-2 hover:bg-gray-200/70 transition-colors
              flex justify-between">
-                <div className="w-1/4 max-w-xs h-auto">
+                <div className="w-24 max-w-xs h-auto">
                     <img src = {img}/>
                 </div>
-                <div className = "flex flex-col items-center">
-                    <div className="text-lg md:text-2xl font-bold">  
+                <div className = "mx-5 w-full flex flex-col items-center justify-center">
+                    <div className="text-base md:text-2xl font-medium self-start">  
                         {title}
                     </div>
-                    <div className="text-base mt-3">  
+                    <div className="text-xs mt-3 font-light self-end">  
                         <p>
                             {date}
                         </p>

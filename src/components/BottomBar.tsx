ecf51b67f6358d478826d2e36cb29acc051d20eb
nthGH1030 <nth1030@gmail.com>
2025-12-20 23:52:51 +0800

    feat: adding github and linkedln link

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -10,11 +10,11 @@ const BottomBar: React.FC = () => {
     return (
         <section className = "text-3xl text-gray-600 hover:text-black transition-colors">
             <ExtLink
-                href = ""
+                href = "https://github.com/nthGH1030"
                 icon = {<FaGithub/>}
             />
             <ExtLink
-                href = ""
+                href = "www.linkedin.com/in/kelvin-ngan-3b098a1a3"
                 icon = {<FaLinkedin/>}
             />
         </section>

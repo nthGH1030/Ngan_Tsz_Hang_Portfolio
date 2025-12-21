0213b3b870d58ed950c23df379c8be7ec6ab7408
nthGH1030 <nth1030@gmail.com>
2025-12-21 11:18:52 +0800

    feat: Finish reference section

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -8,15 +8,23 @@ import ExtLink from "./extLink";
 const BottomBar: React.FC = () => {
 
     return (
-        <section className = "text-3xl text-gray-600 hover:text-black transition-colors">
-            <ExtLink
-                href = "https://github.com/nthGH1030"
-                icon = {<FaGithub/>}
-            />
-            <ExtLink
-                href = "www.linkedin.com/in/kelvin-ngan-3b098a1a3"
-                icon = {<FaLinkedin/>}
-            />
+        
+        <section>
+            <div className = "hidden md:block">
+                This is for desktop version
+            </div>
+            <div className ="md:hidden text-3xl text-gray-600 hover:text-black transition-colors
+                flex items-center justify-center gap-8 mb-8">
+                <ExtLink
+                    href = "https://github.com/nthGH1030"
+                    icon = {<FaGithub/>}
+                />
+                <ExtLink
+                    href = "www.linkedin.com/in/kelvin-ngan-3b098a1a3"
+                    icon = {<FaLinkedin/>}
+                />
+            </div>
+   
         </section>
     )
 }

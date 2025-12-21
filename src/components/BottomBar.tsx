000913acff419d91c1d4a5c5d98ca9f4e1fbbadd
nthGH1030 <nth1030@gmail.com>
2025-12-21 11:37:09 +0800

    feat: adding bottomBar at desktop mode

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -8,12 +8,24 @@ import ExtLink from "./extLink";
 const BottomBar: React.FC = () => {
 
     return (
-        
         <section>
             <div className = "hidden md:block">
-                This is for desktop version
+                <div className = "fixed bottom-0 left-0 ml-12">
+                    <div className ="text-2xl text-gray-600 hover:text-black transition-colors
+                        flex flex-col items-center justify-center gap-8 mb-4">
+                        <ExtLink
+                            href = "https://github.com/nthGH1030"
+                            icon = {<FaGithub/>}
+                        />
+                        <ExtLink
+                            href = "www.linkedin.com/in/kelvin-ngan-3b098a1a3"
+                            icon = {<FaLinkedin/>}
+                        />
+                    </div>
+                    <div className = "bg-gray-300 h-24 w-px mx-2"></div>
+                </div>
             </div>
-            <div className ="md:hidden text-3xl text-gray-600 hover:text-black transition-colors
+            <div className ="md:hidden text-4xl text-gray-600 hover:text-black transition-colors
                 flex items-center justify-center gap-8 mb-8">
                 <ExtLink
                     href = "https://github.com/nthGH1030"

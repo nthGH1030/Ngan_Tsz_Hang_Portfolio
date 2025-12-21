ec02afd63dfe1ded56245f3bd4df745d3a2c29be
nthGH1030 <nth1030@gmail.com>
2025-12-21 12:31:04 +0800

    feat: setting the close button and margin correctly

--- src/components/HamMenu.tsx
+++ src/components/HamMenu.tsx
@@ -17,10 +17,17 @@ const HamMenu: React.FC = () => {
             />
             {isMOpen ? 
                 <aside className = "fixed top-0 right-0 z-50 top-0 h-full w-3/4 bg-gray-300">
-                    lol
                     <IoIosClose 
+                        className = "absolute top-4 right-4 text-5xl"
                         onClick = {() => setIsMOpen(false)}
                     />
+                    <div className = "flex flex-col items-center justify-center gap-2 text-lg my-64 mx-6">
+                        <a href = "#about" className = "nav-link" >About</a>
+                        <a href = "#experience" className = "nav-link" >Experience</a>
+                        <a href = "#qualification" className = "nav-link" >Qualification</a>
+                        <a href = "#work" className = "nav-link" >Work</a>
+                        <a href = "#contact" className = "nav-link" >Contact</a>
+                    </div>
                 </aside> 
             : ""}
         </div>

fd257a362c5f469829380d1db8754f2434d4fc9c
nthGH1030 <nth1030@gmail.com>
2025-12-21 17:23:39 +0800

    feat: smooth scrool into each anchor

--- src/components/HamMenu.tsx
+++ src/components/HamMenu.tsx
@@ -10,25 +10,55 @@ const HamMenu: React.FC = () => {
         console.log("i am clicked")
     }
 
+    const handleNavClick = (e: React.MouseEvent<HTMLAnchorElement> , target: string) => {
+        e.preventDefault();
+        setIsMOpen(false);
+        setTimeout(() => {
+            const el = document.querySelector(target);
+            if(el){
+                el.scrollIntoView({behavior: 'smooth'})
+            }
+        },150)
+    }
+
     return (
         <div>
             <GiHamburgerMenu 
                 onClick = {handleMOpen}
             />
             {isMOpen ? 
-                <aside className = "fixed top-0 right-0 z-50 top-0 h-full w-3/4 bg-gray-300">
-                    <IoIosClose 
-                        className = "absolute top-4 right-4 text-5xl"
-                        onClick = {() => setIsMOpen(false)}
-                    />
-                    <div className = "flex flex-col items-center justify-center gap-2 text-lg my-64 mx-6">
-                        <a href = "#about" className = "nav-link" >About</a>
-                        <a href = "#experience" className = "nav-link" >Experience</a>
-                        <a href = "#qualification" className = "nav-link" >Qualification</a>
-                        <a href = "#work" className = "nav-link" >Work</a>
-                        <a href = "#contact" className = "nav-link" >Contact</a>
-                    </div>
-                </aside> 
+                <div>
+                    <div className = "backdrop-blur fixed top-0 left-0 z-45 top-0 h-full w-full"></div>
+                    <aside className = "fixed top-0 right-0 z-50 top-0 h-full w-3/4 bg-gray-300">
+                        <IoIosClose 
+                            className = "absolute top-4 right-4 text-5xl"
+                            onClick = {() => setIsMOpen(false)}
+                        />
+                        <div className ="flex flex-col items-center justify-center gap-8 
+                            text-4xl my-64 mx-6 scrool-smooth">
+                            <a href = "#about" 
+                                className = "nav-link" 
+                                onClick = {(e) => handleNavClick(e, "#about")}>
+                                About</a>
+                            <a href = "#experience" 
+                                className = "nav-link" 
+                                onClick = {(e) => handleNavClick(e, "#experience")}>
+                                Experience</a>
+                            <a href = "#qualification" 
+                                className = "nav-link" 
+                                onClick = {(e) => handleNavClick(e, "#qualification")}>
+                                Qualification</a>
+                            <a href = "#work" 
+                                className = "nav-link" 
+                                onClick = {(e) => handleNavClick(e, "#work")}>
+                                Work</a>
+                            <a href = "#contact" 
+                                className = "nav-link" 
+                                onClick = {(e) => handleNavClick(e, "#contact")}>
+                                Contact</a>
+                        </div>
+                    </aside> 
+                </div>
             : ""}
         </div>
     )

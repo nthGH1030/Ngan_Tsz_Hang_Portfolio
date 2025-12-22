495e22287a073f9a5c222a4c05b25d9ec163cba2
nthGH1030 <nth1030@gmail.com>
2025-12-22 23:13:26 +0800

    fix: proper slide in and slide out, no delay on blur, proper spin on icon

--- src/components/HamMenu.tsx
+++ src/components/HamMenu.tsx
@@ -3,23 +3,23 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
 
 const HamMenu: React.FC = () => {
-    const [isMOpen , setIsMOpen] = useState(false)
+    const [isClosing , setisClosing] = useState(false)
     const [isVisible, setIsVisible] = useState(false);
 
     const handleOpen = () => {
         setIsVisible(true);
-        setTimeout(() => setIsMOpen(true), 10)
+        setisClosing(true)
     }
+    
 
     const handleClose = () => {
-        setIsVisible(false);
-        setTimeout(() => setIsMOpen(false), 10)
+        setisClosing(false)
     }
 
     const handleNavClick = (e: React.MouseEvent<HTMLAnchorElement> , target: string) => {
         e.preventDefault();
         setIsVisible(false)
-        setIsMOpen(false);
+        setisClosing(false);
         setTimeout(() => {
             const el = document.querySelector(target);
             if(el){
@@ -28,15 +28,15 @@ const HamMenu: React.FC = () => {
         },150)
     }
 
-    const menuSlide = isMOpen ? "animate-slide-in" : "animate-slide-out"
-    const blur = isMOpen ? "backdrop-blur" : ""
+    const menuSlide = isClosing ? "animate-slide-in" : "animate-slide-out"
+    const blur = isVisible ? "backdrop-blur" : ""
 
     return (
         <div> 
             <div>
                 <GiHamburgerMenu 
                     className = {`fixed top-12 right-12 text-5xl z-50
-                        ${isMOpen 
+                        ${isClosing 
                             ? isVisible ? `opacity-0 scale-90 pointer-events-none animate-spin-fade-out` 
                             : 'opacity-0 scale-90 pointer-events-none'
                             : isVisible ? 'opacity-100 animate-spin-fade-in' : 'opacity-100'
@@ -45,19 +45,27 @@ const HamMenu: React.FC = () => {
                 />
                 <IoClose 
                     className = {`fixed top-12 right-12 text-5xl z-51
-                        ${isMOpen 
+                        ${isClosing 
                             ? isVisible ? `opacity-100 animate-spin-fade-in ` 
                             :  'opacity-100'
                             : isVisible ? 'opacity-0 scale-90 pointer-events-none animate-spin-fade-out' : 
                             'opacity-0 scale-90 pointer-events-none'
                     }`} 
+                    //Click -> !isClosing -> Trigger Animation -> setIsvisible(false)
+                    onAnimationEnd = {() => {
+                        if(!isClosing) setIsVisible(false)
+                    }}
                     onClick={handleClose}
                 />
             </div>
             {isVisible ? 
                 <div>
                     <div className = {`${blur} fixed top-0 left-0 z-45 top-0 h-full w-full`}></div>
-                    <aside className = {`fixed top-0 right-0 z-50 top-0 h-full w-3/4 bg-gray-300 ${menuSlide}`}>
+                    <aside className = {`fixed top-0 right-0 z-50 top-0 h-full w-3/4 bg-gray-300 ${menuSlide}`}
+                        onAnimationEnd = {() => {
+                            if(!isClosing) setIsVisible(false)
+                        }}
+                    >
                         <div className ="flex flex-col items-center justify-center gap-8 
                             text-4xl my-64 mx-6 scrool-smooth">
                             <a href = "#about" 

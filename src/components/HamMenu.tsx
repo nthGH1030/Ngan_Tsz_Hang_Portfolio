4a0ac526c5db7586f6fcf7c4e6307aef4518cd31
nthGH1030 <nth1030@gmail.com>
2025-12-25 11:47:59 +0800

    feat : use react portal to render overlay menu

--- src/components/HamMenu.tsx
+++ src/components/HamMenu.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IoClose } from "react-icons/io5";
+import {createPortal} from 'react-dom'
 
 const HamMenu: React.FC = () => {
     const [isOpen , setisOpen] = useState(false)
@@ -42,23 +43,24 @@ const HamMenu: React.FC = () => {
                     }`} 
                     onClick={handleOpen}
                 />
-                <IoClose 
-                    className = {`fixed top-12 right-12 text-5xl z-30
-                        ${isOpen 
-                            ? isVisible ? `opacity-100 animate-spin-fade-in ` 
-                            :  'opacity-100'
-                            : isVisible ? 'opacity-0 scale-90 pointer-events-none animate-spin-fade-out' : 
-                            'opacity-0 scale-90 pointer-events-none'
-                    }`} 
-                    //Click -> !isOpen -> Trigger Animation -> setIsvisible(false)
-                    onAnimationEnd = {() => {
-                        if(!isOpen) setIsVisible(false)
-                    }}
-                    onClick={handleClose}
-                />
+                
             </div>
-            {isVisible ? 
+            {isVisible && createPortal( 
                 <div>
+                    <IoClose 
+                        className = {`fixed top-12 right-12 text-5xl z-30
+                            ${isOpen 
+                                ? isVisible ? `opacity-100 animate-spin-fade-in ` 
+                                :  'opacity-100'
+                                : isVisible ? 'opacity-0 scale-90 pointer-events-none animate-spin-fade-out' : 
+                                'opacity-0 scale-90 pointer-events-none'
+                        }`} 
+                        //Click -> !isOpen -> Trigger Animation -> setIsvisible(false)
+                        onAnimationEnd = {() => {
+                            if(!isOpen) setIsVisible(false)
+                        }}
+                        onClick={handleClose}
+                    />
                     <div className = {`${blur} fixed top-0 left-0 z-20 top-0 h-full w-full`}></div>
                     <aside className = {`fixed top-0 right-0 z-20 top-0 h-full w-3/4 bg-gray-300 ${menuSlide}`}>
                         <div className ="flex flex-col items-center justify-center gap-8 
@@ -90,8 +92,8 @@ const HamMenu: React.FC = () => {
                         </div>
                     </aside> 
                 </div>
-            : 
-            ""}
+                , document.body
+            )}
         </div>
     )
 }

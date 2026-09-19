16b4bb6d977132fb414e848ada682ab6c4945cf4
nthGH1030 <nth1030@gmail.com>
2026-09-19 23:06:31 +0800

    feat: remove bottom bar

--- src/components/navigation/BottomBar.tsx
+++ src/components/navigation/BottomBar.tsx
@@ -26,19 +26,6 @@ const BottomBar: React.FC = () => {
                     <div className = "bg-line h-24 w-px mx-2" aria-hidden="true"></div>
                 </div>
             </div>
-            <div className ="md:hidden text-4xl text-muted
-                flex items-center justify-center gap-2 mb-8">
-                <ExternalLink
-                    href = "https://github.com/nthGH1030"
-                    ariaLabel="GitHub profile"
-                    icon = {<FaGithub aria-hidden="true" />}
-                />
-                <ExternalLink
-                    href = "https://www.linkedin.com/in/kelvin-ngan-3b098a1a3"
-                    ariaLabel="LinkedIn profile"
-                    icon = {<FaLinkedin aria-hidden="true" />}
-                />
-            </div>
    
         </aside>
     )
